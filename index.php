906bab3bc458e48528d80174cacfcc2e69c2eb6e
crissilvafr <cris.silvaf7@gmail.com>
2023-08-19 17:25:00 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cristian Silva</title>
+</head>
+<body style="margin: 0px;
+padding: 0%;
+background-image: url('spheres.jpg');
+background-position: center;
+background-size: cover;
+background-repeat: no-repeat;">
+    <div style="width: 100%; height: 100vh; color: white; background-color: rgba(31, 31, 31, .5);">
+        <center>
+            <h1>Hello, World!</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit necessitatibus iure consequatur officiis eaque alias tenetur iste aperiam accusamus nobis tempora ipsa excepturi eius culpa, aliquid quidem error eum ad!</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo veritatis ducimus sunt deleniti doloremque. Temporibus, praesentium repellendus. Consequuntur tempora, reiciendis, veniam ab nemo enim vero aliquid nostrum dolore, quidem possimus.</p>
+            </center>
+    </div>
+    
+</body>
+</html>
